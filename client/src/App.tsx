8b47f34239826ce75322269d8bb85af5ee5f52a5
aksworns22 <wkdeogks17@gmail.com>
2026-08-26 17:48:49 +0900

    충돌 해결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { Header } from './components/Header';
+import { ProductListing } from './components/ProductListing';
 import './styles/global.css';
 
 function App() {
     return (
         <div className="app-shell">
             <Header />
-            <main className="app-shell__main" />
+            <main className="app-shell__main">
+                <ProductListing />
+            </main>
         </div>
     );
 }
