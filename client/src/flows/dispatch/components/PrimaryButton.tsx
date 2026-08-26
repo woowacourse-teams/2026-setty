e27e35447c04fa9505b05f44b18c44f8d6724e4c
aksworns22 <wkdeogks17@gmail.com>
2026-08-26 14:46:21 +0900

    refactor: 기존 클라이언트 프로젝트를 제거한다

--- client/src/flows/dispatch/components/PrimaryButton.tsx
+++
@@ -1,9 +0,0 @@
-import type { ButtonHTMLAttributes } from 'react';
-import styles from './PrimaryButton.module.css';
-
-type PrimaryButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
-
-/** 시안의 56px 초록 주요 action 버튼 */
-export default function PrimaryButton({ type = 'button', ...props }: PrimaryButtonProps) {
-  return <button className={styles.button} type={type} {...props} />;
-}
