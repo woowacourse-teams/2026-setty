c6ea73047e7002c1e6d0343785f8f435260a01ae
aksworns22 <wkdeogks17@gmail.com>
2026-09-21 15:55:30 +0900

    feat: 로그인을 위한 ui 구현

--- android/app/src/main/java/com/aksworns22/setty/MainActivity.kt
+++ android/app/src/main/java/com/aksworns22/setty/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedSecureTextField
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aksworns22.setty.ui.theme.SettyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +35,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SettyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SignInScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SignInScreen() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier
+            .systemBarsPadding()
+            .padding(top = 32.dp, start = 32.dp, end = 32.dp, bottom = 16.dp)
+            .imePadding()
+    ) {
+        Text(text = "Setty 로그인", style = MaterialTheme.typography.headlineLargeEmphasized)
+        Text(
+            text = "세티와 함께 중고 가구 거래를 시작해보세요.",
+            style = MaterialTheme.typography.bodyLargeEmphasized
+        )
+        OutlinedTextField(
+            state = rememberTextFieldState(),
+            label = { Text("세티 아이디", style = MaterialTheme.typography.bodyMediumEmphasized) },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            lineLimits = TextFieldLineLimits.SingleLine,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        OutlinedSecureTextField(
+            state = rememberTextFieldState(),
+            label = { Text("세티 비밀번호", style = MaterialTheme.typography.bodyMediumEmphasized) },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "계정 생성하기",
+                style = MaterialTheme.typography.bodyLargeEmphasized,
+                modifier = Modifier.clickable {},
+            )
+            Button(onClick = {}) {
+                Text("로그인", style = MaterialTheme.typography.bodyLargeEmphasized)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+private fun SignInScreenPreview() {
     SettyTheme {
-        Greeting("Android")
+        SignInScreen()
     }
 }
